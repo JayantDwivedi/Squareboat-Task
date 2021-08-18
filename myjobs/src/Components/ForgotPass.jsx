d1f21d71135c4f4da627958415ba05e2ef627e61
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-08-18 16:46:44 +0530

    done

--- myjobs/src/Components/ForgotPass.jsx
+++ myjobs/src/Components/ForgotPass.jsx
@@ -1,51 +1,62 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import './CSS/form.css';
+import "./CSS/form.css";
 
 export default function ForgotPass() {
+  const [email, setEmail] = useState();
+  const [error, setError] = useState();
 
-    const [email, setEmail] = useState();
-    const [error, setError] = useState();
+  const emailRegex = /\S+@\S+\.\S+/;
 
-    const emailRegex = /\S+@\S+\.\S+/;
-
-    const handleSubmit=()=>{
-        if (validate(email)) {
-           alert(email);
-           setEmail("");
-           setError("");
-        }
+  const handleSubmit = () => {
+    if (validate(email)) {
+      console.log(email);
+      setEmail("");
+      setError("");
     }
+  };
 
-    const validate=(email)=>{
-        if (email == undefined) {
-            setError("");
-            setError("Feild is mandatory");
-        }
-        else if (!emailRegex.test(email)) {
-            setError("");
-            setError("Enter a valid email");
-        }
-        else {
-            return true;
-        }
+  const validate = (email) => {
+    if (email == undefined) {
+      setError("");
+      setError("Feild is mandatory");
+    } else if (!emailRegex.test(email)) {
+      setError("");
+      setError("Enter a valid email");
+    } else {
+      return true;
     }
+  };
 
-    return(
-        <>
-            <div className="main-body">
-                <h3>Forgot Password?</h3>
-                <p>Enter the email associated with your account and we'll send you instruction to reset your password</p>
-                <label htmlFor="email">Email address<sup>*</sup></label>
-                <br />
-                <input value={email} type="email" className="input-box p-2 my-3 rounded" placeholder="Enter your email" onChange={(e)=>{setEmail(e.target.value)}} />
-                <br />
-                <p className="error">{error}</p>
-                <div className="text-center">
-                <button className="btn btn-primary px-4" onClick={handleSubmit}>Submit</button>
-                </div>
-                
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="main-body">
+        <h3>Forgot Password?</h3>
+        <p>
+          Enter the email associated with your account and we'll send you
+          instruction to reset your password
+        </p>
+        <label htmlFor="email">
+          Email address<sup>*</sup>
+        </label>
+        <br />
+        <input
+          value={email}
+          type="email"
+          className="input-box p-2 my-3 rounded"
+          placeholder="Enter your email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <p className="error">{error}</p>
+        <div className="text-center">
+          <button className="btn btn-primary px-4" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
